e97be8415c0bfa4aa9bc53a61d4a3452e81cb4c4
lersatromtsova <v.stromtsova@yandex.ru>
2019-09-11 23:01:50 +0300

    Performed some linting.

--- students/lerastromtsova/1/runtests.py
+++ students/lerastromtsova/1/runtests.py
@@ -3,18 +3,32 @@ import subprocess
 
 PREFIX_TESTS = 'test_'
 FILE_TYPE_TESTS = '.py'
-SAMPLES_PATH = '/Users/gingy/ITMO/4_course/testing/itmo-2019/students/lerastromtsova/1/samples/'
+SAMPLES_PATH = '/Users/gingy/ITMO/4_course/testing/itmo-2019' \
+               '/students/lerastromtsova/1/samples/'
 
-class TestRunner:
+
+class TestRunner(object):
+    """
+        Class for running tests in any given directory.
+
+        Test filenames should start with PREFIX defined above
+        and have an extension FILE_TYPE_TESTS defined above
+    """
 
     tests = []
     passed = []
     failed = []
 
     def collect_tests(self, path):
-        self.tests = glob.glob(f'{path}/{PREFIX_TESTS}*{FILE_TYPE_TESTS}')
+        """ Collects the tests from a given path. """
+        self.tests.extend(glob.glob(f'{path}/{PREFIX_TESTS}*{FILE_TYPE_TESTS}'))
 
     def run_tests(self):
+        """
+            Runs the tests from self.tests.
+
+            Adds them to either passed or failed.
+        """
         for test in self.tests:
             try:
                 subprocess.check_call(f'python {test}', shell=True)
@@ -25,7 +39,7 @@ class TestRunner:
                 self.failed.append(test)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     test_runner = TestRunner()
     test_runner.collect_tests(SAMPLES_PATH)
     test_runner.run_tests()
--- students/lerastromtsova/1/test_runtests.py
+++ students/lerastromtsova/1/test_runtests.py
@@ -1,19 +1,22 @@
 import unittest
 from runtests import TestRunner
 
-SAMPLES_PATH = '/Users/gingy/ITMO/4_course/testing/itmo-2019/students/lerastromtsova/1/samples/'
+SAMPLES_PATH = '/Users/gingy/ITMO/4_course/testing/itmo-2019' \
+               '/students/lerastromtsova/1/samples/'
 NON_EXISTING_PATH = '/Users/gingy/sometests'
 NO_TESTS_PATH = '/Users/gingy/ITMO/4_course/testing'
 
 
 class Test(unittest.TestCase):
+    """ Class for performing unit tests on my testing framework. """
 
     def test_collect_tests(self):
+        """ Test the 'collect_tests' method """
 
         # test happy path
         test_runner = TestRunner()
         test_runner.collect_tests(SAMPLES_PATH)
-        self.assertEqual(test_runner.tests, [SAMPLES_PATH+'test_factorial.py', SAMPLES_PATH+'test_gcd.py'])
+        self.assertEqual(test_runner.tests, [SAMPLES_PATH + 'test_factorial.py', SAMPLES_PATH + 'test_gcd.py'])
 
         # test if path does not exist
         test_runner = TestRunner()
@@ -24,13 +27,15 @@ class Test(unittest.TestCase):
         test_runner = TestRunner()
         test_runner.collect_tests(NO_TESTS_PATH)
         self.assertEqual(test_runner.tests, [])
-        
+
     def test_run_tests(self):
+        """ Test the 'run_tests' method """
+
         test_runner = TestRunner()
         test_runner.collect_tests(SAMPLES_PATH)
         test_runner.run_tests()
-        self.assertEqual(test_runner.passed, [SAMPLES_PATH+'test_factorial.py'])
-        self.assertEqual(test_runner.failed, [SAMPLES_PATH+'test_gcd.py'])
+        self.assertEqual(test_runner.passed, [SAMPLES_PATH + 'test_factorial.py'])
+        self.assertEqual(test_runner.failed, [SAMPLES_PATH + 'test_gcd.py'])
 
 
 if __name__ == '__main__':
