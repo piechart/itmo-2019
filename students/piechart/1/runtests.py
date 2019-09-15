7ec1fdce7fd2f7f7b5e7d1f3083e8034d700bcdf
Michael Galperin <mgalp.connect@gmail.com>
2019-09-15 15:37:47 +0300

    Updated homework 1

--- students/piechart/1/runtests.py
+++ students/piechart/1/runtests.py
@@ -1,56 +1,70 @@
-import os
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import glob
 import imp
+import os
 
-PY_EXTENSION = ".py"
-TEST_PREFIX = "test_"
+PY_EXTENSION = '.py'
+TEST_PREFIX = 'test_'
 
 
 def find_files(directory, pattern):
-    return glob.glob(f"{directory}/{pattern}", recursive=True)
+    """Returns list of filepaths."""
+    return glob.glob('{0}/{1}'.format(directory, pattern), recursive=True)
 
 
 def import_module(target_file):
-    global PY_EXTENSION
-    name = os.path.basename(target_file).replace(PY_EXTENSION, "")
+    """Returns either imported module or None."""
+    name = os.path.basename(target_file).replace(PY_EXTENSION, '')
 
-    if len(name) == 0 or (not os.path.exists(target_file)):
+    if (not name) or (not os.path.exists(target_file)):
         return None
 
-    module = imp.load_source(name, target_file)
-    return module
+    return imp.load_source(name, target_file)
 
 
 def extract_tests(module):
-    global TEST_PREFIX
+    """Returns either funcs dict or None."""
     if module is None:
         return None
-    return dict(filter(lambda elem: elem[0].startswith(TEST_PREFIX), vars(module).items()))
+
+    all_vars = vars(module).items()  # noqa: WPS421
+    tests = filter(lambda elem: elem[0].startswith(TEST_PREFIX), all_vars)
+    return dict(tests)
 
 
-def perform_testing(target_dir=""):
-    if len(target_dir) == 0:
+def execute_test(test_function):
+    """Executes a function given."""
+    try:
+        test_function()
+    except AssertionError:
+        return 'fail'
+    return 'ok'
+
+
+def perform_testing(target_dir=''):
+    """Looks up for tests in the directory given and performs its execution."""
+    if not target_dir:
         target_dir = os.getcwd()
 
-    test_files = find_files(target_dir, f"{TEST_PREFIX}*{PY_EXTENSION}")
-
-    for test_file in test_files:
+    for test_file in find_files(
+        target_dir,
+        '{0}*{1}'.format(TEST_PREFIX, PY_EXTENSION),
+    ):
         module = import_module(test_file)
 
         if module is None:
             continue
 
-        tests = extract_tests(module)
-
-        for test in tests.items():
-            try:
-                test[1]()
-                test_result = "ok"
-            except AssertionError:
-                test_result = "fail"
-
-            print(f"{test_file} - {test[0]} - {test_result}")
+        for test in (extract_tests(module) or {}).items():
+            print('{0} - {1} - {2}'.format(  # noqa: T001
+                test_file,
+                test[0],
+                execute_test(test[1]),
+            ))
 
 
 if __name__ == '__main__':
-    perform_testing(input("Input target dir (Enter for current): "))
+    target_dir = input('Input target dir (Enter for current): ')  # noqa: WPS421, S322, E501
+    perform_testing(target_dir)
--- students/piechart/1/test_runtests.py
+++ students/piechart/1/test_runtests.py
@@ -1,32 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import os
+
 import runtests
 
 
 def test_correct_module_import():
-    assert runtests.import_module(os.getcwd() + "/file_with_tests.py") != None
+    assert runtests.import_module(
+        '{0}/{1}'.format(os.getcwd(), 'file_with_tests.py'),
+    )
 
 
 def test_incorrect_module_import():
-    assert runtests.import_module("non_existing_file.py") == None
+    assert (not runtests.import_module('non_existing_file.py'))
 
 
 def test_find_valid_files():
-    assert len(runtests.find_files(os.getcwd(), "*.py")) > 0
+    assert runtests.find_files(
+        os.getcwd(), '*{0}'.format(runtests.PY_EXTENSION),
+    )
 
 
 def test_find_invalid_files():
-    assert len(runtests.find_files(os.getcwd(), "*.invalidExtension")) == 0
+    assert (not runtests.find_files(os.getcwd(), '*.invalidExtension'))
 
 
 def test_extract_tests():
-    module = runtests.import_module(os.getcwd() + "/file_with_tests.py")
-    assert runtests.extract_tests(module) != None
-    assert bool(runtests.extract_tests(module))  # not empty
-    assert bool(runtests.extract_tests(None)) == False  # empty
-
-
-test_correct_module_import()
-test_incorrect_module_import()
-test_find_valid_files()
-test_find_invalid_files()
-test_extract_tests()
+    module = runtests.import_module(
+        '{0}/{1}'.format(os.getcwd(), 'file_with_tests.py'),
+    )
+    assert runtests.extract_tests(module)
+    assert (not runtests.extract_tests(None))
