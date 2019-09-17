473449d4ed43732a1fad93dfb551552713774798
Michael Galperin <mgalp.connect@gmail.com>
2019-09-17 21:26:07 +0300

    flaked test_runtests.py

--- students/piechart/1/test_runtests.py
+++ students/piechart/1/test_runtests.py
@@ -7,26 +7,31 @@ import runtests
 
 
 def test_correct_module_import():
+    """Tests correct module import."""
     assert runtests.import_module(
         '{0}/{1}'.format(os.getcwd(), 'file_with_tests.py'),
     )
 
 
 def test_incorrect_module_import():
+    """Tests incorrect module import."""
     assert (not runtests.import_module('non_existing_file.py'))
 
 
 def test_find_valid_files():
+    """Tests valid files finding."""
     assert runtests.find_files(
         os.getcwd(), '*{0}'.format(runtests.PY_EXTENSION),
     )
 
 
 def test_find_invalid_files():
+    """Tests invalid files finding."""
     assert (not runtests.find_files(os.getcwd(), '*.invalidExtension'))
 
 
 def test_extract_tests():
+    """Tests funcs extractions."""
     module = runtests.import_module(
         '{0}/{1}'.format(os.getcwd(), 'file_with_tests.py'),
     )
