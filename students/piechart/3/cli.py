798302708380a770e5fd77e29b302992ceb642c5
Michael Galperin <mgalp.connect@gmail.com>
2019-09-25 18:01:46 +0300

    I hate flake

--- students/piechart/3/cli.py
+++ students/piechart/3/cli.py
@@ -1,32 +1,44 @@
+# -*- coding: utf-8 -*-
+
 import argparse
 import os
 import sys
 
+WRONG_ARGUMENT = 'wrong argument'
+
+
 def main(commands, module):
-    if hasattr(module, commands[0]):
+    """Main."""
+    if hasattr(module, commands[0]):  # noqa WPS421
         arg = commands[1] if len(commands) > 1 else None
-        getattr(module, commands[0])(arg)
+        getattr(module, commands[0])(arg)  # noqa WPS421
     else:
-        print('Unknown command passed')
+        print('Unknown command passed')  # noqa T001
+
 
 def ls(arg=None):
-    d = os.getcwd() if arg is None else arg
-    files = [f.name for f in os.scandir(d) if f.is_file()]
-    dirs = [folder.name for folder in os.scandir(d) if folder.is_dir()]
+    """ls."""
+    directory = os.getcwd() if arg is None else arg
+    files = [file_object.name for file_object in os.scandir(directory) if file_object.is_file()]  # noqa E501
+    dirs = [folder.name for folder in os.scandir(directory) if folder.is_dir()]
     return files + dirs
 
+
 def mk(arg=None):
+    """mk."""
     if not arg:
-        return 'wrong argument'
+        return WRONG_ARGUMENT
     if os.path.exists(arg):
         return 'file already exists'
     try:
-        open(arg, 'a').close()
+        open(arg, 'a').close()  # noqa WPS515
     except OSError:
         return 'invalid filename'
     return 'success'
 
+
 def rm(arg=None):
+    """rm."""
     if not arg:
         return 'wrong argument'
     if os.path.isdir(arg):
@@ -36,32 +48,40 @@ def rm(arg=None):
     os.remove(arg)
     return 'success'
 
+
 def contains(arg=None):
+    """contains."""
     if not arg:
-        return 'wrong argument'
+        return WRONG_ARGUMENT
     if os.path.isdir(arg):
         return 'argument is dir'
     return 0 if arg in ls() else 1
 
-def since(timestamp=None, directory=os.getcwd()):
+
+def since(timestamp=None, directory=os.getcwd()):  # noqa WPS404, B008
+    """since."""
     if not timestamp:
-        return 'wrong argument'
+        return WRONG_ARGUMENT
     try:
         timestamp = int(timestamp)
-    except:
-        return 'wrong argument'
+    except Exception:
+        return WRONG_ARGUMENT
     if not os.path.exists(directory):
         return 'dir not found'
-    content = ls(directory)
-    if not content:
+    dir_content = ls(directory)
+    if not dir_content:
         return 'dir is empty'
-    return [item for item in content if os.stat('{0}/{1}'.format(directory, item)).st_ctime > timestamp]
-
+    return [
+        item_object for item_object in dir_content if os.stat('{0}/{1}'.format(
+            directory,
+            item_object,
+        )).st_ctime > timestamp
+    ]
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('command', type=str, nargs='*', help='Command to execute')
+    parser.add_argument('command', type=str, nargs='*', help='Command')
     args = parser.parse_args()
 
     current_module = sys.modules[__name__]
--- students/piechart/3/test_cli.py
+++ students/piechart/3/test_cli.py
@@ -1,148 +1,213 @@
-from cli import (
-    ls,
-    mk,
-    rm,
-    contains,
-    since,
-)
+# -*- coding: utf-8 -*-
+
 
 import pytest
 import subprocess
 
+from cli import (
+    contains,
+    ls,
+    mk,
+    rm,
+    since,
+)
+
+YES_THIS_IS_MAGIC_NUMBER = 123
+
+
 @pytest.fixture
 def earlier_than_now_timestamp():
-    from datetime import datetime
+    """Timestamp."""
+    from datetime import datetime  # noqa WPS433
     return int(datetime.timestamp(datetime.now())) - 10
 
+
 def test_ls_empty_dir(tmp_path):
-    assert ls(tmp_path) == []
+    """Test func."""
+    assert not ls(tmp_path)
+
 
 def test_ls_only_dirs(tmp_path):
-    d = tmp_path / 'another_dir'
-    d.mkdir()
+    """Test func."""
+    directory = tmp_path / 'another_dir'
+    directory.mkdir()
     assert len(ls(tmp_path)) == 1
+
 
 def test_ls_only_files(tmp_path):
-    f = tmp_path / 'file.txt'
-    f.write_text('text')
-    assert f.read_text() == 'text'
+    """Test func."""
+    filename = tmp_path / 'file.txt'
+    filename.write_text('text')
+    assert filename.read_text() == 'text'
     assert len(ls(tmp_path)) == 1
 
+
 def test_ls_files_and_dirs(tmp_path):
-    d = tmp_path / 'sub'
-    d.mkdir()
-    f = tmp_path / 'file.txt'
-    f.write_text('text')
-    assert(len(ls(tmp_path))) == 2
+    """Test func."""
+    directory = tmp_path / 'sub'
+    directory.mkdir()
+    filename = tmp_path / 'file.txt'
+    filename.write_text('text')
+    assert len(ls(tmp_path)) == 2
+
 
 def test_ls_integration():
+    """Test func."""
     assert subprocess.call(['python3', 'cli.py', 'ls']) == 0
 
+
 def test_mk_no_filename():
+    """Test func."""
     assert mk() == 'wrong argument'
 
+
 def test_mk_en_filename(tmp_path):
-    f = tmp_path / 'file.txt'
-    assert mk(f) == 'success'
+    """Test func."""
+    filename = tmp_path / 'file.txt'
+    assert mk(filename) == 'success'
+
 
 def test_mk_ru_filename(tmp_path):
-    f = tmp_path / 'файл.txt'
-    assert mk(f) == 'success'
+    """Test func."""
+    filename = tmp_path / 'файл.txt'
+    assert mk(filename) == 'success'
+
 
 def test_mk_integration():
+    """Test func."""
     assert subprocess.call(['python3', 'cli.py', 'mk', 'file.txt']) == 0
 
+
 def test_mk_duplicate(tmp_path):
-    f = tmp_path / 'file.txt'
-    f.write_text('text')
-    assert mk(f) == 'file already exists'
+    """Test func."""
+    filename = tmp_path / 'file.txt'
+    filename.write_text('text')
+    assert mk(filename) == 'file already exists'
+
 
 def test_mk_invalid_filename(tmp_path):
-    f = tmp_path / 'f/1/l/e.txt'
-    assert mk(f) == 'invalid filename'
+    """Test func."""
+    filename = tmp_path / 'f/1/l/e.txt'
+    assert mk(filename) == 'invalid filename'
+
 
 def test_rm_success(tmp_path):
-    f = tmp_path / 'file.txt'
-    f.write_text('text')
-    assert rm(f) == 'success'
+    """Test func."""
+    filename = tmp_path / 'file.txt'
+    filename.write_text('text')
+    assert rm(filename) == 'success'
+
 
 def test_rm_dir(tmp_path):
-    d = tmp_path / 'dir'
-    d.mkdir()
-    assert rm(d) == 'argument is dir'
+    """Test func."""
+    directory = tmp_path / 'dir'
+    directory.mkdir()  # noqa WPS204
+    assert rm(directory) == 'argument is dir'
+
 
 def test_rm_fail(tmp_path):
-    f = tmp_path / 'somefile.txt'
-    assert rm(f) == 'file not found'
+    """Test func."""
+    filename = tmp_path / 'somefile.txt'
+    assert rm(filename) == 'file not found'
+
 
 def test_rm_no_filename():
+    """Test func."""
     assert rm() == 'wrong argument'
 
+
 def test_rm_integration():
+    """Test func."""
     assert subprocess.call(['python3', 'cli.py', 'rm', 'somefile.txt']) == 0
 
+
 def test_contains_no_filename():
+    """Test func."""
     assert contains() == 'wrong argument'
 
+
 def test_contains_success():
+    """Test func."""
     mk('file.txt')
     assert contains('file.txt') == 0
     rm('file.txt')
 
+
 def test_contains_dir(tmp_path):
-    d = tmp_path / 'dir'
-    d.mkdir()
-    assert contains(d) == 'argument is dir'
+    """Test func."""
+    directory = tmp_path / 'dir'
+    directory.mkdir()  # noqa WPS204
+    assert contains(directory) == 'argument is dir'
+
 
 def test_contains_non_existing_file():
+    """Test func."""
     assert contains('non_existing_file.txt') == 1
 
+
 def test_since_no_date():
+    """Test func."""
     assert since() == 'wrong argument'
 
+
 def test_contains_integration():
+    """Test func."""
     assert subprocess.call(['python3', 'cli.py', 'contains', 'file.txt']) == 0
 
+
 def test_since_not_existing_dir(tmp_path):
-    d = tmp_path / 'dir'
-    assert since(123, d) == 'dir not found'
+    """Test func."""
+    directory = tmp_path / 'dir'
+    assert since(YES_THIS_IS_MAGIC_NUMBER, directory) == 'dir not found'
+
 
 def test_since_empty_dir(tmp_path):
-    d = tmp_path / 'dir'
-    d.mkdir()
-    assert since(123, d) == 'dir is empty'
+    """Test func."""
+    directory = tmp_path / 'dir'
+    directory.mkdir()
+    assert since(YES_THIS_IS_MAGIC_NUMBER, directory) == 'dir is empty'
 
-def test_since_only_dirs(tmp_path, earlier_than_now_timestamp):
-    d = tmp_path / 'dir'
-    d.mkdir()
-    ad = d / 'another dir'
-    ad.mkdir()
-    result = since(earlier_than_now_timestamp, d)
-    assert isinstance(result, list)
-    assert len(result) == 1
 
-def test_since_only_files(tmp_path, earlier_than_now_timestamp):
-    d = tmp_path / 'dir'
-    d.mkdir()
-    f = d / 'file.txt'
-    f.write_text('test')
-    result = since(earlier_than_now_timestamp, d)
-    assert isinstance(result, list)
-    assert len(result) == 1
+def test_since_only_dirs(tmp_path, earlier_than_now_timestamp):  # noqa WPS442
+    """Test func."""
+    directory = tmp_path / 'dir'
+    directory.mkdir()
+    adirectory = directory / 'another dir'
+    adirectory.mkdir()
+    func_result = since(earlier_than_now_timestamp, directory)
+    assert isinstance(func_result, list)
+    assert len(func_result) == 1
 
-def test_since_dirs_and_files(tmp_path, earlier_than_now_timestamp):
-    d = tmp_path / 'dir'
-    d.mkdir()
-    subdir = d / 'subdir'
+
+def test_since_only_files(tmp_path, earlier_than_now_timestamp):  # noqa WPS442
+    """Test func."""
+    directory = tmp_path / 'dir'
+    directory.mkdir()
+    filename = directory / 'file.txt'
+    filename.write_text('test')
+    func_result = since(earlier_than_now_timestamp, directory)
+    assert isinstance(func_result, list)
+    assert len(func_result) == 1
+
+
+def test_since_dirs_and_files(tmp_path, earlier_than_now_timestamp):  # noqa WPS442
+    """Test func."""
+    directory = tmp_path / 'dir'
+    directory.mkdir()
+    subdir = directory / 'subdir'
     subdir.mkdir()
-    f = d / 'file.txt'
-    f.write_text('text')
-    result = since(earlier_than_now_timestamp, d)
-    assert isinstance(result, list)
-    assert len(result) == 2
+    filename = directory / 'file.txt'
+    filename.write_text('text')
+    func_result = since(earlier_than_now_timestamp, directory)
+    assert isinstance(func_result, list)
+    assert len(func_result) == 2
+
 
 def test_since_integration():
+    """Test func."""
     assert subprocess.call(['python3', 'cli.py', 'since', '0']) == 0
 
+
 def test_since_invalid_date():
+    """Test func."""
     assert since('abcd') == 'wrong argument'
