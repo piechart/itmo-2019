8f5e201162a1e20ca4b7280950bbda00dcb6e174
lersatromtsova <v.stromtsova@yandex.ru>
2019-10-25 11:00:52 +0300

    Added actual logic to the usecases module

--- students/lerastromtsova/6/pizzaz/fyodorpizza/usecases/get_menu.py
+++ students/lerastromtsova/6/pizzaz/fyodorpizza/usecases/get_menu.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
 
+from fyodorpizza.models import Pizza
+from fyodorpizza.serializers import PizzaSerializer
+
 
 class GetMenu(object):
     """
-       Gets menu - a list of all :term:`Pizza`.
+    Gets menu - a list of all :term:`Pizza`.
 
-       .. literalinclude:: user_stories/get_menu.feature
-         :language: gherkin
+    .. literalinclude:: user_stories/get_menu.feature
+      :language: gherkin
 
-       .. versionadded:: 0.1.0
+    .. versionadded:: 0.1.0
     """
 
     def __call__(self):
-        pass
+        """Method to call when a user wants to get :term:`Pizza` menu."""
+        pizzas = Pizza.objects.all()
+        serializer = PizzaSerializer(pizzas, many=True)
+        return serializer.data
--- students/lerastromtsova/6/pizzaz/fyodorpizza/usecases/get_statistics.py
+++ students/lerastromtsova/6/pizzaz/fyodorpizza/usecases/get_statistics.py
@@ -3,13 +3,14 @@
 
 class GetStatistics(object):
     """
-       Gets statistics on :term:`Pizza` ordered today.
+    Gets statistics on :term:`Pizza` ordered today.
 
-       .. literalinclude:: user_stories/get_statistics.feature
-         :language: gherkin
+    .. literalinclude:: user_stories/get_statistics.feature
+      :language: gherkin
 
-       .. versionadded:: 0.1.0
+    .. versionadded:: 0.1.0
     """
 
     def __call__(self):
-        pass
+        """Method to call when a user wants to see statistics."""
+        pass  # noqa: WPS420
