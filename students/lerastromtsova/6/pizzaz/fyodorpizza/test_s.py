76be34b23e00ee4ad144ac6750e8098ac020d51f
lersatromtsova <v.stromtsova@yandex.ru>
2019-10-25 11:00:15 +0300

    Extended tests

--- students/lerastromtsova/6/pizzaz/fyodorpizza/test_s.py
+++ students/lerastromtsova/6/pizzaz/fyodorpizza/test_s.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
 
-from django.test import TestCase
-from django.test import Client
-from .models import Ingredient, Pizza, Order
-from decimal import Decimal
-from .serializers import PizzaSerializer
 import json
+from decimal import Decimal
 
+from django.test import Client, TestCase
 
-OK = 200
-TEST_PRICE = Decimal(1.00)
+from fyodorpizza.models import Ingredient, Order, Pizza
+from fyodorpizza.response_codes import BAD_REQUEST, CREATED, OK
+from fyodorpizza.usecases.get_menu import GetMenu
+
+TEST_ADDRESS = 'Petrogradskaya emb., 36A, SPb, Russia'
+TEST_EMAIL = 'v.stromtsova@yandex.ru'
+
+TEST_PRICE = 1
+TEST_PRICE_DEC = Decimal(TEST_PRICE)
 
 
 def create_test_ingredient(name):
@@ -42,6 +46,8 @@ def create_test_order(pizza_list, delivery_address, client_email):
 
 
 class GetMenuTest(TestCase):
+    """Test get_menu usecase."""
+
     client = Client()
 
     def setUp(self):
@@ -49,23 +55,28 @@ class GetMenuTest(TestCase):
         ingredient_test = create_test_ingredient('test')
         ingredient_cheese = create_test_ingredient('cheese')
 
-        self.pizza = create_test_pizza(name='Test with cheese',
-                                       price=TEST_PRICE,
-                                       ingredient_list=[ingredient_cheese,
-                                                        ingredient_test])
+        self.pizza = create_test_pizza(
+            name='Test with cheese',
+            price=TEST_PRICE_DEC,
+            ingredient_list=[
+                ingredient_cheese,
+                ingredient_test,
+            ],
+        )
 
     def test_can_get_menu(self):
         """Check that API returns list of :term:`Pizza`."""
         response = self.client.get(path='/api/pizza/')
-        assert response.status_code == 200
+        assert response.status_code == OK
 
-        pizzaserializer = PizzaSerializer([self.pizza], many=True)
-        ser_data = json.dumps(pizzaserializer.data)
+        ser_data = json.dumps(GetMenu()())
         resp_data = response.content.decode()
         assert ser_data == resp_data
 
 
 class PostOrderTest(TestCase):
+    """Tests post_order usecase."""
+
     client = Client()
 
     def setUp(self):
@@ -73,21 +84,27 @@ class PostOrderTest(TestCase):
         ingredient_test = create_test_ingredient('test')
         ingredient_cheese = create_test_ingredient('cheese')
 
-        self.pizza = create_test_pizza(name='Test with cheese',
-                                       price=TEST_PRICE,
-                                       ingredient_list=[ingredient_cheese,
-                                                        ingredient_test])
+        self.pizza = create_test_pizza(
+            name='Test with cheese',
+            price=TEST_PRICE_DEC,
+            ingredient_list=[
+                ingredient_cheese,
+                ingredient_test,
+            ],
+        )
 
     def test_can_post_order(self):
-        """Make POST request and assert that :term:`Order` is saved in database and email is sent."""
-        response = self.client.post(path='/api/order/',
-                                    data={
-                                        'status': 'accept',
-                                        'pizza_list': [self.pizza.pk],
-                                        'delivery_address': 'Petrogradskaya emb., 36A, Saint Petersburg, Russia',
-                                        'client_email': 'v.stromtsova@yandex.ru',
-                                    })
-        assert response.status_code == 201
+        """Make POST request and assert status code 201 and email is sent."""
+        response = self.client.post(
+            path='/api/order/',
+            data={
+                'status': 'accept',
+                'pizza_list': [self.pizza.pk],
+                'delivery_address': TEST_ADDRESS,
+                'client_email': TEST_EMAIL,
+            },
+        )
+        assert response.status_code == CREATED
 
         # assert saving to db
         resp_data = json.loads(response.content.decode())
@@ -97,42 +114,45 @@ class PostOrderTest(TestCase):
         # assert email_is_sent on order
         assert order.email_is_sent
 
-        # assert cooking time
-
     def test_cannot_post_invalid_order(self):
-        """Make POST request with invalid params and assert that :term:`Order` is not saved in database."""
-        response = self.client.post(path='/api/order/',
-                                    data={
-                                        'pizza_list': [],
-                                        'delivery_address': 'Petrogradskaya emb., 36A, Saint Petersburg, Russia',
-                                        'client_email': 'v.stromtsova@yandex.ru',
-                                    })
+        """Make POST request with invalid params and assert status code 401."""
+        response = self.client.post(
+            path='/api/order/',
+            data={
+                'pizza_list': [],
+                'delivery_address': TEST_ADDRESS,
+                'client_email': TEST_EMAIL,
+            },
+        )
 
-        assert response.status_code == 400
-        # assert order not saved to db
+        assert response.status_code == BAD_REQUEST
 
 
 class GetStatisticsTest(TestCase):
+    """Tests get_statistics usecase."""
+
     client = Client()
 
     def setUp(self):
         """Make test :term:`Order`."""
         self.order = create_test_order(
-            pizza_list=[create_test_pizza(
-                name='Sausage with tomato',
-                price=TEST_PRICE,
-                ingredient_list=[
-                    create_test_ingredient('tomato'),
-                    create_test_ingredient('sausage'),
-                ]
-            )],
-            delivery_address='Petrogradskaya emb., 36A, Saint Petersburg, Russia',
-            client_email='v.stromtsova@yandex.ru',
+            pizza_list=[
+                create_test_pizza(
+                    name='Sausage with tomato',
+                    price=TEST_PRICE_DEC,
+                    ingredient_list=[
+                        create_test_ingredient('tomato'),
+                        create_test_ingredient('sausage'),
+                    ],
+                ),
+            ],
+            delivery_address=TEST_ADDRESS,
+            client_email=TEST_EMAIL,
         )
 
     def test_can_get_statistics(self):
         """Make GET request and check status code and :term:`Pizza` list."""
         response = self.client.get(path='/api/statistics/pizza/')
 
-        assert response.status_code == 200
+        assert response.status_code == OK
         # assert that received list matches list of pizzas in order
