107d1883482164a039ddc424e248b9bdd43c85ad
Michael Galperin <mgalp.connect@gmail.com>
2019-10-29 19:29:56 +0300

    Started API

--- students/piechart/6/main/views/api.py
+++ students/piechart/6/main/views/api.py
@@ -2,28 +2,46 @@ import json
 
 from django.http import JsonResponse
 from main.models import *
+from datetime import strptime
 
-# def get_airlines(request):
-#     id = request.GET.get("id")
-#     if id is not None:
-#         response = {
-#             "airline": Airline.objects.get(pk=id).as_dict()
-#         }
-#     else:
-#         response = {
-#             "airlines": [airline.as_dict() for airline in Airline.objects.all()]
-#         }
-#     return JsonResponse(response)
+def get_pizzas(request):
+    response = {
+        "pizzas": [pizza.as_dict() for pizza in Pizza.objects.all()]
+    }
+    return JsonResponse(response)
+    
+def create_order(request):
+    def resolve_pizza(pizza_title):
+        pizza = Pizza.objects.get(title=pizza_title)
+        if pizza is not None:
+            return pizza
+        return None
+        
+    date = request.POST.get('date')
+    address = request.POST.get('address')
+    email = request.POST.get('email')
+    
+    pizza_titles = request.POST.get('pizzas').split(',')
+    pizza_items = map(lambda title: resolve_pizza(title), pizza_titles)
+    pizzas = [pizza for pizza in pizza_items if pizza is not None]
+    if not pizzas:
+        return JsonResponse({
+            "result": "error",
+            "error": "no valid pizzas provided"
+        })
+    
+    order = Order(
+        "place_date": strptime(date, '%y-%m-%d'),
+        "pizzas": pizzas,
+        "delivery_address": address,
+        "customer_email": email,
+        "status": "ACCEPTED"
+    )
+    order.save()
+    return JsonResponse({
+        "result": "success",
+        "order_id": order.pk
+    })
 
-# def delete_airline(request):
-#     id = request.GET.get("id")
-#     if id is not None:
-#         try:
-#             airline = Airline.objects.get(pk=int(id))
-#         except:
-#             return JsonResponse({"result": "error", "error": "airline not found"})
-# 
-#         airline.delete()
-#         return JsonResponse({"result": "success"})
-#     else:
-#         return JsonResponse({"result": "error", "error": "no id provided"})
+def stats(request):
+    TODO
